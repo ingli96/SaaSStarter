fb1988531743197dd009e0e7ec632a810590753a
ingli96 <aalexkireev@gmail.com>
2024-11-12 18:00:08 -0800

    test_pricing

--- src/routes/(marketing)/pricing/pricing_plans.ts
+++ src/routes/(marketing)/pricing/pricing_plans.ts
@@ -12,32 +12,32 @@ export const pricingPlans = [
   },
   {
     id: "pro",
-    name: "Pro",
+    name: "pro",
     description:
-      "A plan to test the purchase experience. Try buying this with the test credit card 4242424242424242.",
-    price: "$5",
+      "just for test",
+    price: "$10",
     priceIntervalName: "per month",
-    stripe_price_id: "price_1NkdZCHMjzZ8mGZnRSjUm4yA",
-    stripe_product_id: "prod_OXj1CcemGMWOlU",
+    stripe_price_id: "price_1QKVMNA3KM9m25OLSK6p3bnN",
+    stripe_product_id: "prod_RCvCSsBLD2ImT5",
     features: [
       "Everything in Free",
       "Support us with fake money",
       "Test the purchase experience",
     ],
   },
-  {
-    id: "enterprise",
-    name: "Enterprise",
-    description:
-      "A plan to test the upgrade experience. Try buying this with the test credit card 4242424242424242.",
-    price: "$15",
-    priceIntervalName: "per month",
-    stripe_price_id: "price_1Nkda2HMjzZ8mGZn4sKvbDAV",
-    stripe_product_id: "prod_OXj20YNpHYOXi7",
-    features: [
-      "Everything in Pro",
-      "Try the 'upgrade plan' UX",
-      "Still actually free!",
-    ],
-  },
+  // {
+  //   id: "enterprise",
+  //   name: "Enterprise",
+  //   description:
+  //     "A plan to test the upgrade experience. Try buying this with the test credit card 4242424242424242.",
+  //   price: "$15",
+  //   priceIntervalName: "per month",
+  //   stripe_price_id: "price_1Nkda2HMjzZ8mGZn4sKvbDAV",
+  //   stripe_product_id: "prod_OXj20YNpHYOXi7",
+  //   features: [
+  //     "Everything in Pro",
+  //     "Try the 'upgrade plan' UX",
+  //     "Still actually free!",
+  //   ],
+  // },
 ]
